6cca6a5cd40be327c73ba1d9ec76083a0ad0f85b
mehdirt <mahdirafati680@gmail.com>
2025-01-26 23:40:09 +0330

    Feat: Data cleaning implementation completed

---
+++ src/data_cleaning.py
@@ -0,0 +1,87 @@
+import logging
+from abc import ABC, abstractmethod
+from typing import Union
+
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+class DataStrategy(ABC):
+    """
+    Abstract class defining strategy for handling data.
+    """
+
+    @abstractmethod
+    def handle_data(self, data: pd.DataFrame) -> Union[pd.DataFrame, pd.Series]:
+        pass
+
+class DataPreProcessStrategy(DataStrategy):
+    """
+    Strategy for preprocessing data.
+    """
+    
+    def handle_data(self, data: pd.DataFrame) -> Union[pd.DataFrame, pd.Series]:
+        """
+        Preprocess the data.
+        """
+        try:
+            # Drop unnecessary features
+            data = data.drop([
+                "order_approved_at",
+                "order_delivered_carrier_date",
+                "order_delivered_customer_date",
+                "order_estimated_delivery_date",
+                "order_purchase_timestamp",
+            ], axis=1)
+            # Fill null values of features
+            data["product_weight_g"].fillna(data["product_weight_g"].median(), inplace=True)
+            data["product_length_cm"].fillna(data["product_length_cm"].median(), inplace=True)
+            data["product_height_cm"].fillna(data["product_height_cm"].median(), inplace=True)
+            data["product_weight_cm"].fillna(data["product_weight_cm"].median(), inplace=True)
+            data["review_comment_message"].fillna("No review", inplace=True)
+            # Select cumeric features
+            data = data.select_dtypes(include=[np.number])
+            # Drop unwanted numeric features
+            cols_to_drop = ["customer_zip_code_prefix", "order_item_id"]
+            data = data.drop(cols_to_drop, axis=1)
+            return data
+        
+        except Exception as err:
+            logging.error(f"Error in preprocessing data: {err}")
+            raise err
+        
+class DataSplitStrategy(DataStrategy):
+    """
+    Strategy for Splitting data into train and test.
+    """
+    def handle_data(self, data: pd.DataFrame) -> Union[pd.DataFrame, pd.Series]:
+        """
+        Split data into train and test.
+        """
+        try:
+            X = data.drop(["review_score"], axis=1)
+            y = data["review_score"]
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2, random_state=42)
+            return X_train, X_test, y_train, y_test
+
+        except Exception as err:
+            logging.error("Error in splitting data: {err}")
+            raise err
+        
+class DataCleaning():
+    """
+    Class for cleaning data which processes the data and divides it into train and test. 
+    """
+    def __init__(self, data: pd.DataFrame, strategy: DataStrategy):
+        self.data = data
+        self.strategy = strategy
+
+    def handle_data(self) -> Union[pd.DataFrame, pd.Series]:
+        """
+        Handles cleaning data.
+        """
+        try:
+            return self.strategy.handle_data(self.data)
+        except Exception as err:
+            logging.error(f"Error in handling data: {err}")
+            raise err
--- steps/clean_data.py
+++ steps/clean_data.py
@@ -2,8 +2,60 @@ import logging
 import pandas as pd
 from zenml import step
 
+from src.data_cleaning import DataCleaning, DataPreProcessStrategy, DataSplitStrategy
+from typing_extensions import Annotated
+from typing import Tuple
+
 @step
-def clean_dframe(dframe: pd.DataFrame) -> None: #! Must be changed tp pd.DataFrame
+def clean_dframe(dframe: pd.DataFrame) -> Tuple[
+    Annotated[pd.DataFrame, "X_train"],
+    Annotated[pd.DataFrame, "X_test"],
+    Annotated[pd.Series, "y_train"],
+    Annotated[pd.Series, "y_test"],
+]: 
     """
+    Cleans and preprocesses the input dataframe, then splits it into training and testing datasets.
+
+    This function performs the following tasks:
+    1. Preprocesses the input dataframe by handling missing values, encoding categorical features,
+       and applying other data cleaning strategies.
+    2. Splits the cleaned dataframe into features (X_train, X_test) and target labels (y_train, y_test)
+       for training and testing the machine learning model.
+
+    Args:
+        dframe (pd.DataFrame): The input data to be cleaned and split.
+
+    Returns:
+        Tuple[Annotated[pd.DataFrame, "X_train"], Annotated[pd.DataFrame, "X_test"], 
+               Annotated[pd.Series, "y_train"], Annotated[pd.Series, "y_test"]]:
+            - X_train: The training features (cleaned).
+            - X_test: The testing features (cleaned).
+            - y_train: The training target labels.
+            - y_test: The testing target labels.
+
+    Raises:
+        Exception: If there is an error during the data cleaning or splitting process.
+
+    Example:
+        X_train, X_test, y_train, y_test = clean_dframe(my_dataframe)
+
     """
-    pass
+    try:
+        # Preprocess the data
+        process_strategy = DataPreProcessStrategy()
+        data_cleaning = DataCleaning(dframe, strategy=process_strategy)
+        processed_data = data_cleaning.handle_data()
+        logging.info("Data preprocessing finished")
+
+        # Split the data into train/test
+        split_strategy = DataSplitStrategy()
+        data_cleaning = DataCleaning(dframe, strategy=DataSplitStrategy)
+        X_train, X_test, y_train, y_test = data_cleaning.handle_data()
+        logging.info("Data splitting finished")
+        logging.info("Data Cleaning Completed")
+
+    except Exception as err:
+        logging.error(f"Error occuered in cleaning data: {err}")
+        raise err
+
+    
