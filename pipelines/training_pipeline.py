3a4179fd32731cef3eacdce0b458503dbf2897d1
mehdirt <mahdirafati680@gmail.com>
2025-01-26 20:09:59 +0330

    BugFix: refactoring to the previous state, no need to config Pydantic model

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -4,7 +4,7 @@ from steps.clean_data import clean_dframe
 from steps.model_train import train_model
 from steps.evaluation import evaluate_model
 
-@pipeline
+@pipeline(enable_cache=False)
 def train_pipeline(data_path: str):
     """
     """
--- steps/model_train.py
+++ steps/model_train.py
@@ -1,17 +1,9 @@
 import logging
 import pandas as pd
 from zenml import step
-from pydantic import BaseModel
-
-# Create a Pydantic model with arbitrary_types_allowed
-class DataFrameConfig(BaseModel):
-    df: pd.DataFrame
-
-    class Config:
-        arbitrary_types_allowed = True
 
 @step
-def train_model(dframe: DataFrameConfig) -> None:
+def train_model(dframe: pd.DataFrame) -> None:
     """
     """
     pass
